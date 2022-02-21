a1dac246260ba32505fec7fc39ceeab86cf27faf
NaN-Simon <goosaimon@gmail.com>
2022-02-21 13:51:43 +0300

    feat&fix: start new starRate

--- src/formElements/sup-block/second-column/button-rate/button-rate.js
+++ src/formElements/sup-block/second-column/button-rate/button-rate.js
@@ -1,28 +1,25 @@
 /* const rateOne = document.querySelectorAll(".button-rate-one");
-const buttonRateColor = document.querySelectorAll(".button-rate-color");
 
-  rateOne.forEach((element) => {
-    element.addEventListener("click", function (e) {
-      console.log(showElement(e));
-      element[showElement(e)].innerHTML = 'star'
-  })
-})
+rateOne.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    console.log(showElement(e));
+  });
+});
 function showElement(e) {
   var t = e.target;
-  var checkboxes = t.parentNode.getElementsByClassName("button-rate-color");
+  var checkboxes = t.parentNode.getElementsByClassName("button-rate-one");
   var result = Array.prototype.indexOf.call(checkboxes, t);
   return result;
-} */
-let firstRate = 4
-let parg = document.querySelector(".button-rate-one");
-if (parg.hasChildNodes()) {
-  let children = parg.childNodes[1].textContent;
-  console.log(children)
-  for (let i = 0; i<firstRate;i++){
-    console.log('go')
+}
+let firstRate = 4;
+let buttonRateOne = document.querySelector(".button-rate-one");
+if (buttonRateOne.hasChildNodes()) {
+  let children = buttonRateOne.childNodes[0].textContent;
+  console.log(children);
+  for (let i = 0; i < firstRate; i++) {
+    console.log("go");
     document.querySelector(".button-rate-one").childNodes[i].innerHTML = "star";
   }
-  
-}
+} */
 
-/* console.log(document.getElementsByClassName(".button-rate-one").childNodes[0].innerHTML) */
+let buttonRateOne = document.querySelector(".button-rate-one");
