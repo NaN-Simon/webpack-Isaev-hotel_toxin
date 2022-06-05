97be33101aa5f075d1a806ff84b421264defa19f
NaN-Simon <goosaimon@gmail.com>
2022-06-05 12:32:56 +0300

    feat: add count innerHTML

--- src/components-v2/dropdown/dropdown.js
+++ src/components-v2/dropdown/dropdown.js
@@ -1,10 +1,12 @@
-let temp = () => {
+let temp = (data = [], placeholder) => {
+  const placeholderText = placeholder ?? 'Выберите колличество гостей';
+  // console.log(data)
   return `
   <div class="dropdown__title">
     <div class="tit3">DROPDOWN</div>
     </div>
   <div class="dropdown__title-list active" data-type="input">
-    Сколько гостей
+    ${placeholderText}
     <div class="material-icons">
       <span class="dropdown__btn" data-type="input">expand_more</span>
     </div>
@@ -18,8 +20,22 @@ let temp = () => {
     <div class="dropdown__item-plus">+</div>
   </div>
   </li>
-    <li class="dropdown__item">Два</li>
-    <li class="dropdown__item">Три</li>
+  <li class="dropdown__item">
+  <div class="dropdown__item-type">Кровати</div>
+  <div class="dropdown__item-counter">
+    <div class="dropdown__item-minus">-</div>
+    <div class="dropdown__item-vault">0</div>
+    <div class="dropdown__item-plus">+</div>
+  </div>
+  </li>
+  <li class="dropdown__item">
+  <div class="dropdown__item-type">Ванные комнаты</div>
+  <div class="dropdown__item-counter">
+    <div class="dropdown__item-minus">-</div>
+    <div class="dropdown__item-vault">0</div>
+    <div class="dropdown__item-plus">+</div>
+  </div>
+  </li>
   </div>
   `;
 };
@@ -27,28 +43,56 @@ let temp = () => {
 class Dropdown {
   constructor(selector, options) {
     this.$dropdown = document.querySelector(selector);
+    this.options = options;
     this.#render();
     this.#setup();
   }
 
   #render() {
-    this.$dropdown.innerHTML = temp();
+    const { placeholder, data } = this.options;
+    this.$dropdown.innerHTML = temp(data, placeholder);
   }
   #setup() {
     this.clickHandler = this.clickHandler.bind(this);
     this.$dropdown.addEventListener('click', this.clickHandler);
+    this.$arrow = this.$dropdown.querySelector('.dropdown__btn');
   }
+  clickHandler(event, options) {
+    //обработка клика - выдвижение списка элментов, смена кнопки
+    const { type } = event.target.dataset;
+    if (type == 'input') {
+      event.target.closest('.dropdown__title-list').classList.toggle('active');
+      this.$arrow.innerHTML == 'expand_more'
+        ? (this.$arrow.innerHTML = 'expand_less')
+        : (this.$arrow.innerHTML = 'expand_more');
+    }
 
-  clickHandler(event) {
+    //обработка клика - смена результата в элементах списка
+    const { data } = this.options;
+    this.$dropdownItem = this.$dropdown.querySelectorAll('.dropdown__item');
+    const current = event.target.closest('.dropdown__item');
+    const minus = current.querySelector('.dropdown__item-minus');
+    const vault = current.querySelector('.dropdown__item-vault');
+    const plus = current.querySelector('.dropdown__item-plus');
 
-     const {type} = event.target.dataset;
-     
-     if(type == 'input'){
-      event.target.closest('.dropdown__title-list').classList.toggle('active')
-     }
+    console.log(data)
+    if (vault.innerHTML >= 0) {
+      event.target === plus ? vault.innerHTML++ : false;
+    }
+    if (vault.innerHTML > 0) {
+      event.target === minus ? vault.innerHTML-- : false;
+    }
 
+    //TODO при нажатии менять data score
   }
 }
 
-const dropdown = new Dropdown('#dropdown', {});
+const dropdown = new Dropdown('#dropdown', {
+  placeholder: 'Сколько гостей',
+  data: [
+    { id: '0', score: 0, value: 'Спальни' },
+    { id: '1', score: 0, value: 'Кровати' },
+    { id: '2', score: 0, value: 'Ванные комнаты' },
+  ],
+});
 window.dropdown = dropdown;
